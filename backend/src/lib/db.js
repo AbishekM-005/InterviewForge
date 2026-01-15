ed3104dc3fe46a24664417b99f19ff5ddef362ae
NiCaRoZ <143282515+NiCaRoZ@users.noreply.github.com>
2026-01-15 15:32:41 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import ENV from "./env.js";
+
+const connectDB = async () => {
+  try {
+    const conn = mongoose.connect(ENV.DB_URL);
+    console.log("Connected successfully: ", conn);
+  } catch (error) {
+    console.error("Error connecting to MongoDB ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
